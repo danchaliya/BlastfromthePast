2399147e0053ff7ac78b40c4e431f9b2246dbe19
Purav Gupta <guptapurav@gmail.com>
2024-01-28 01:55:06 -0500

    flask updates

--- GPT-4.py
+++ GPT-4.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import base64
 import requests
 from flask_cors import CORS, cross_origin
@@ -14,55 +14,57 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['POST','GET'])  # this sets the route to this page
 # @cross_origin(supports_credentials=True)
+
+# def gpt4():
+#     api_key = "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"
+
+#   # Function to encode the image
+#     def encode_image(image_path):
+#         with open(image_path, "rb") as image_file:
+#             return base64.b64encode(image_file.read()).decode('utf-8')
+
+#     # Path to your image
+#     image_path = "/Users/puravgupta/Downloads/IMG_1206.jpeg"
+
+#     # Getting the base64 string
+#     base64_image = encode_image(image_path)
+
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {api_key}"
+#     }
+
+#     payload = {
+#         "model": "gpt-4-vision-preview",
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": [
+#                     {
+#                         "type": "text",
+#                         "text": "What’s in this image?"
+#                     },
+#                     {
+#                         "type": "image_url",
+#                         "image_url": {
+#                             "url": f"data:image/jpeg;base64,{base64_image}"
+#                         }
+#                     }
+#                 ]
+#             }
+#         ],
+#         "max_tokens": 300
+#     }
+
+#     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+#     content = response.json()['choices'][0]['message']['content']
+
+#     print("post is working")
+
+#     return response.json(), f"Here is the summary of the image: {content}"  
+
 def home():
-      # OpenAI API Key
-  api_key = "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"
-
-  # Function to encode the image
-  def encode_image(image_path):
-      with open(image_path, "rb") as image_file:
-          return base64.b64encode(image_file.read()).decode('utf-8')
-
-  # Path to your image
-  image_path = "/Users/puravgupta/Downloads/IMG_1206.jpeg"
-
-  # Getting the base64 string
-  base64_image = encode_image(image_path)
-
-  headers = {
-      "Content-Type": "application/json",
-      "Authorization": f"Bearer {api_key}"
-  }
-
-  payload = {
-      "model": "gpt-4-vision-preview",
-      "messages": [
-          {
-              "role": "user",
-              "content": [
-                  {
-                      "type": "text",
-                      "text": "What’s in this image?"
-                  },
-                  {
-                      "type": "image_url",
-                      "image_url": {
-                          "url": f"data:image/jpeg;base64,{base64_image}"
-                      }
-                  }
-              ]
-          }
-      ],
-      "max_tokens": 300
-  }
-
-  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-  content = response.json()['choices'][0]['message']['content']
-
-  print("post is working")
-
-  # return f"Hello! this is the main page <h1>HELLO</h1> {content}"
-  return response.json(), f"Here is the summary of the image: {content}"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
