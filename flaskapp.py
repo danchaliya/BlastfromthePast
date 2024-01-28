c4232b29b8c2a22d1b4b8648fc72d3a0b8d0b8bc
Purav Gupta <guptapurav@gmail.com>
2024-01-28 05:14:45 -0500

    flask button and more style functionality

--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,7 @@ from flask_cors import CORS, cross_origin
 import GPT
 import datetime
 
-# content = GPT.gpt4("/Users/puravgupta/Downloads/IMG_1206.jpeg")
+
 print(datetime.datetime.now())
 
 app = Flask(__name__)
@@ -13,10 +13,19 @@ app = Flask(__name__)
 # CORS(app, support_credentials=True)
 
 # Defining the home page of our site
-@app.route("/", methods = ['POST','GET'])  # this sets the route to this page
+@app.route("/")  # this sets the route to this page
 
 def home():
     return render_template('index.html')
+
+# Defining another page to route to GPT-4
+@app.route("/create/", methods = ['POST'])  # this sets the route to this page
+    
+def gpt4():
+    # content = GPT.gpt4("/Users/puravgupta/Downloads/IMG_1206.jpeg")
+    print("it is working now")
+    return render_template('index.html');
+
 
 
 if __name__ == "__main__":
