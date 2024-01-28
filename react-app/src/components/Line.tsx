fb265e002e7a6f9610598a2d5cc200ed2da3b9d0
amandakt <akytong70@gmail.com>
2024-01-27 22:32:22 -0500

    fixed stuff

---
+++ react-app/src/components/Line.tsx
@@ -0,0 +1,9 @@
+import './Line.css';
+
+const Line = () => {
+  return (
+    <div className="line"></div>
+  );
+}
+
+export default Line;
--- react-app/src/pages/LandingPage.tsx
+++ react-app/src/pages/LandingPage.tsx
@@ -1,21 +1,31 @@
-import Header from '../components/Header';
+//import Header from '../components/Header';
 import Button from '../components/Button';
+import Line from '../components/Line';
 import { Link } from 'react-router-dom';
 import icon from '../assets/icon.png';
 import './landingpage.css';
 function LandingPage() {
     return (
         <div>
-            <Link to="/sign-in">
-            <Button>Log In</Button>
-            </Link>
-            <Header/>
-            <div style={{display:'flex'}}>
-                <div><img src={icon} id="logo"></img></div>
-                <div style={{marginLeft:'auto'}}><button id="loginButton">Login</button></div>
+            <div>
+                <div style={{display:'flex'}}>
+                    <div>
+                        <img src={icon} id="logo"></img>
+                        </div>
+                    <div style={{marginLeft:'auto'}}>
+                        <Link to="/sign-in">
+                            <button id="loginButton">Login</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <h2> Login </h2>
+            <div>
+                <Line />
+                <Line />
+            </div>
+
         </div>
+        
       );
 }
 
