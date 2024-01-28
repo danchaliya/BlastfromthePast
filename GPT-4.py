d809eb21d3c4be07267d821ad6d2c07c7e69a489
amandakt <akytong70@gmail.com>
2024-01-27 23:54:49 -0500

    Revert "integrate hell"
    This reverts commit 92d6124ddb787829de8aa4576c273c4c5ae6c16a.

--- GPT-4.py
+++ GPT-4.py
@@ -1,69 +1,66 @@
 from flask import Flask, jsonify
 import base64
 import requests
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
-CORS(app, support_credentials=True)
 
 # Defining the home page of our site
 # @app.route("/")  # this sets the route to this page
 # def home():
 #     return f"Hello! this is the main page <h1>HELLO</h1> {content}"  # some basic inline html fix
 
-@app.route("/", methods = ['POST','GET'])  # this sets the route to this page
-@cross_origin(supports_credentials=True)
+
+@app.route("/", methods = ['POST'])  # this sets the route to this page
 def home():
       # OpenAI API Key
-  api_key = "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"
+  # api_key = "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"
 
-  # Function to encode the image
-  def encode_image(image_path):
-      with open(image_path, "rb") as image_file:
-          return base64.b64encode(image_file.read()).decode('utf-8')
+  # # Function to encode the image
+  # def encode_image(image_path):
+  #     with open(image_path, "rb") as image_file:
+  #         return base64.b64encode(image_file.read()).decode('utf-8')
 
-  # Path to your image
-  image_path = "/Users/puravgupta/Downloads/IMG_1206.jpeg"
+  # # Path to your image
+  # image_path = "/Users/puravgupta/Downloads/IMG_1206.jpeg"
 
-  # Getting the base64 string
-  base64_image = encode_image(image_path)
+  # # Getting the base64 string
+  # base64_image = encode_image(image_path)
 
-  headers = {
-      "Content-Type": "application/json",
-      "Authorization": f"Bearer {api_key}"
-  }
+  # headers = {
+  #     "Content-Type": "application/json",
+  #     "Authorization": f"Bearer {api_key}"
+  # }
 
-  payload = {
-      "model": "gpt-4-vision-preview",
-      "messages": [
-          {
-              "role": "user",
-              "content": [
-                  {
-                      "type": "text",
-                      "text": "What’s in this image?"
-                  },
-                  {
-                      "type": "image_url",
-                      "image_url": {
-                          "url": f"data:image/jpeg;base64,{base64_image}"
-                      }
-                  }
-              ]
-          }
-      ],
-      "max_tokens": 300
-  }
+  # payload = {
+  #     "model": "gpt-4-vision-preview",
+  #     "messages": [
+  #         {
+  #             "role": "user",
+  #             "content": [
+  #                 {
+  #                     "type": "text",
+  #                     "text": "What’s in this image?"
+  #                 },
+  #                 {
+  #                     "type": "image_url",
+  #                     "image_url": {
+  #                         "url": f"data:image/jpeg;base64,{base64_image}"
+  #                     }
+  #                 }
+  #             ]
+  #         }
+  #     ],
+  #     "max_tokens": 300
+  # }
 
-  response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+  # response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
   # content = response.json()['choices'][0]['message']['content']
 
   print("post is working")
 
   # return f"Hello! this is the main page <h1>HELLO</h1> {content}"
-  return response.json()
+  return "works"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=7473)
+    app.run()
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,16 +3,9 @@ import SignIn from './pages/SignIn';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-async function App(){
-  // const [name, setName] = useState('')
-  // useEffect(() => {
-  //   const response = fetch('http://100.67.67.75:7473/').then
-  //   (response => response.json()).then(data => console.log(data['choices'][0]['message']['content']));
-  //   // const value = response.json();
-  //   // console.log(value);
-  // }, [])
-
-  
+function App(){
+  const [name, setName] = useState('')
+  useEffect(() => {}, [name])
   
   return (
   <div>
